a22747fe015636578c5e6a76ce2f8e8b18221136
Jhon <jhon-3316@hotmail.com>
2023-06-15 21:13:17 -0500

    Imagen del odontologo en el registro

--- app/Http/Controllers/API/RegisterUsersController.php
+++ app/Http/Controllers/API/RegisterUsersController.php
@@ -24,7 +24,7 @@ class RegisterUsersController extends Controller
             'password' => 'required|string|min:8',
             'phone' => 'required|string|numeric|digits:10',
             'address' => 'required|string|max:255',
-            'image' => 'required|image',
+            // 'image' => 'required|image',
         ]);
 
         // variable boolean para verificar que la contraseña coincide con la confirmacion
@@ -38,10 +38,10 @@ class RegisterUsersController extends Controller
                 return response()->json(['error' => 'Contraseña no coincide con la confirmación'], 422);
             }
 
-            $file = $request->file('image');
-            $obj = Cloudinary::upload($file->getRealPath(), ['folder' => 'users']);
-            $public_id = $obj->getPublicId();
-            $url = $obj->getSecurePath();
+            // $file = $request->file('image');
+            // $obj = Cloudinary::upload($file->getRealPath(), ['folder' => 'users']);
+            // $public_id = $obj->getPublicId();
+            // $url = $obj->getSecurePath();
 
             // Crear el nuevo usuario
             $user = User::create([
@@ -54,8 +54,8 @@ class RegisterUsersController extends Controller
                 'phone' => $request->input('phone'),
                 'address' => $request->input('address'),
                 'user_state' => 1, //estado 
-                'profile_picture_id' => $public_id,
-                'profile_picture_url' => $url
+                // 'profile_picture_id' => $public_id,
+                // 'profile_picture_url' => $url
             ]);
             // Retornar una respuesta exitosa
             return response()->json(['message' => 'Usuario registrado exitosamente'], 201);
@@ -134,7 +134,7 @@ class RegisterUsersController extends Controller
                 'password' => 'required|string|min:8',
                 'phone' => 'required|string|numeric|digits:10',
                 'address' => 'required|string|max:255',
-                'image' => 'required|image',
+                // 'image' => 'required|image',
             ]);
 
             // variable boolean para verificar que la contraseña coincide con la confirmacion
@@ -148,10 +148,10 @@ class RegisterUsersController extends Controller
                     return response()->json(['error' => 'Contraseña no coincide con la confirmación'], 422);
                 }
 
-                $file = $request->file('image');
-                $obj = Cloudinary::upload($file->getRealPath(), ['folder' => 'users']);
-                $public_id = $obj->getPublicId();
-                $url = $obj->getSecurePath();
+                // $file = $request->file('image');
+                // $obj = Cloudinary::upload($file->getRealPath(), ['folder' => 'users']);
+                // $public_id = $obj->getPublicId();
+                // $url = $obj->getSecurePath();
 
                 // Crear el nuevo usuario
                 $user = User::create([
@@ -164,8 +164,8 @@ class RegisterUsersController extends Controller
                     'phone' => $request->input('phone'),
                     'address' => $request->input('address'),
                     'user_state' => 1, //estado activo
-                    'profile_picture_id' => $public_id,
-                    'profile_picture_url' => $url
+                    // 'profile_picture_id' => $public_id,
+                    // 'profile_picture_url' => $url
                 ]);
                 // Retornar una respuesta exitosa
                 return response()->json(['message' => 'Usuario registrado exitosamente'], 201);
