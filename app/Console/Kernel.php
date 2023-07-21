d9fdcaf1a9b6b752864ddf8ce91cac785d92e1f4
Jhon Torres <38541491+jhon-torres@users.noreply.github.com>
2023-07-21 12:41:48 -0500

    simulación notificaciones - cron job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Http;
 
 class Kernel extends ConsoleKernel
 {
@@ -13,6 +14,28 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            echo "Hola ";
+            $title = 'titulo de prueba';
+            $body = 'cuerpo de prueba';
+            $deviceToken = 'ExponentPushToken[-ptt0CI1CX1qumfq8mdOYv]'; // aqui token del dispositivo EXPO
+
+            // Ejemplo de envío de notificación utilizando una solicitud HTTP
+        $response = Http::post('https://exp.host/--/api/v2/push/send', [
+            'title' => $title,
+            'body' => $body,
+            'to' => $deviceToken,
+        ]);
+
+        // Procesar la respuesta de la solicitud y manejar errores si es necesario
+        if ($response->successful()) {
+            echo 'Notificación enviada correctamente.';
+            // return response()->json(['message' => 'Notificación enviada correctamente.']);
+        } else {
+            echo 'Error al enviar la notificación.';
+            // return response()->json(['message' => 'Error al enviar la notificación.'], 500);
+        }
+        })->everyMinute();
     }
 
     /**
@@ -20,7 +43,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
