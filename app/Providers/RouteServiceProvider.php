e7f3483ecbe1a651a44618c0bec10c31febef998
Jhon Torres <38541491+jhon-torres@users.noreply.github.com>
2023-08-08 16:24:37 -0500

    rutas de API sin midd v2

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -29,9 +29,8 @@ class RouteServiceProvider extends ServiceProvider
         });
 
         $this->routes(function () {
-            // Route::middleware('api')
-            //     ->prefix('api')
-            //     ->group(base_path('routes/api.php'));
+            Route::prefix('api')
+                ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
